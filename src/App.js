e983ebb929f24b4dd8ce141651a9efd07aebbe40
Mehrdad Azizkhani <mehrdad.azizkhani@gmail.com>
2023-03-02 06:17:01 +0330

    fix: cards data

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
 import Card from "./components/card";
-import { FaRedhat, FaSyringe } from "react-icons/fa";
-import {
-  GiCardPick,
-  GiCardRandom,
-  GiBandit,
-  GiBoltShield,
-  GiCrossbow,
-  GiCrosshair,
-  GiBiceps,
-  GiBlackHandShield,
-  GiDesertEagle,
-} from "react-icons/gi";
+import cards from "./cardsData";
 
 function App() {
   return (
-    <div className="flex flex-wrap gap-1 font-display text-content">
-      <Card title="godfather" mafia={true}>
-        <FaRedhat />
-      </Card>
-      <Card title="joker" mafia={true}>
-        <GiCardRandom />
-      </Card>
-      <Card title="hostage taker" mafia={true}>
-        <GiBandit />
-      </Card>
-      <Card title="doctor" mafia={false}>
-        <FaSyringe />
-      </Card>
-      <Card title="detective" mafia={false}>
-        <GiCardPick />
-      </Card>
-      <Card title="gaurdian" mafia={false}>
-        <GiBoltShield />
-      </Card>
-      <Card title="hunter" mafia={false}>
-        <GiCrossbow />
-      </Card>
-      <Card title="sniper" mafia={false}>
-        <GiCrosshair />
-      </Card>
-      <Card title="Die-hard" mafia={false}>
-        <GiBiceps />
-      </Card>
-      <Card title="rifleman" mafia={false}>
-        <GiDesertEagle />
-      </Card>
+    <div className="flex gap-1 font-display text-content">
+      {cards.map((card) => {
+        return <Card data={card} />;
+      })}
     </div>
   );
 }
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import mafiaLogo from "../../assets/mafia.svg";
-import { GiFingerPrint } from "react-icons/gi";
 
-const Card = ({ title, children, mafia }) => {
+const Card = ({ data, id, key }) => {
   const [flip, setFlip] = useState(false);
   const duration = 1000;
   let timer;
@@ -17,6 +16,7 @@ const Card = ({ title, children, mafia }) => {
 
   return (
     <div
+      key={key}
       onClick={handleFlip}
       className={`group relative flex aspect-[4/6] w-80 cursor-pointer select-none flex-col items-center justify-center transition-all duration-1000 [transform-style:preserve-3d] ${
         flip && "[transform:rotateY(180deg)]"
@@ -25,35 +25,18 @@ const Card = ({ title, children, mafia }) => {
       {/* Side A */}
       <div className="absolute flex h-full w-full flex-col items-center justify-center gap-14 rounded-2xl  [background:linear-gradient(20deg,rgba(0,0,0,1)0%,rgba(56,56,56,1)100%)]">
         <img src={mafiaLogo} alt="mafia" className={`w-1/2`} />
-        <div className="relative text-7xl text-secondary">
-          <GiFingerPrint />
-          <div className="absolute top-0 h-0 overflow-hidden text-content transition-all duration-700 group-hover:h-full">
-            <GiFingerPrint />
-          </div>
-        </div>
       </div>
       {/* Side B */}
-      <div className="absolute flex h-full w-full flex-col items-center justify-center gap-24 rounded-2xl [transform:rotateY(180deg)] [background:linear-gradient(20deg,rgba(0,0,0,1)0%,rgba(56,56,56,1)100%)] [backface-visibility:hidden]">
-        <div className="relative flex aspect-square w-1/2 items-center justify-center rounded-full ring-8 ring-content">
-          <div className="absolute top-[calc(50%-3px)] -left-[20%] flex h-[6px] w-[140%] origin-center rotate-0 justify-end">
-            <div className="h-full w-1/5 bg-content"></div>
-          </div>
-          <div className="absolute top-[calc(50%-3px)] -left-[20%] flex h-[6px] w-[140%] origin-center rotate-90 justify-end">
-            <div className="h-full w-1/5 bg-content"></div>
-          </div>
-          <div className="absolute top-[calc(50%-3px)] -left-[20%] flex h-[6px] w-[140%] origin-center rotate-180 justify-end">
-            <div className="h-full w-1/5 bg-content"></div>
-          </div>
-          <div className="absolute top-[calc(50%-3px)] -left-[20%] flex h-[6px] w-[140%] origin-center -rotate-90 justify-end">
-            <div className="h-full w-1/5 bg-content"></div>
-          </div>
-          <div className={`${mafia ? "text-mafia" : "text-citizen"} text-7xl`}>
-            {children}
-          </div>
-        </div>
-        <h2 className="text-2xl font-extrabold uppercase text-heading">
-          {title}
+      <div className="absolute flex h-full w-full flex-col items-center justify-between rounded-2xl p-5 [transform:rotateY(180deg)] [background:linear-gradient(20deg,rgba(0,0,0,1)0%,rgba(56,56,56,1)100%)] [backface-visibility:hidden]">
+        <h2 className="text-center text-xl font-extrabold uppercase text-heading">
+          {data.role}
         </h2>
+        <p className="text-center">{data.desciption}</p>
+        <div
+          className={`h-0.5 w-1/2 animate-pulse ${
+            data.ally === "mafia" ? "bg-mafia" : "bg-citizen"
+          }`}
+        ></div>
       </div>
     </div>
   );
